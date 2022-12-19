d86d393c6a926caeb083d501f962b529d6a61193
IT-sun <orochimaro969@yandex.ru>
2022-12-20 01:41:53 +0300

    init project

---
+++ src/components/users.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import api from "../api";
+
+const Users = () => {
+  const [users, setUsers] = useState(api.users.fetchAll());
+  const handleDelete = (userId) => {};
+  const renderPhrase = (number) => {};
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+export default Users;
